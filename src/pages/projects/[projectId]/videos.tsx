d81666dbe8b355ec0d6c4a4bf16e1cb8e605eccf
Hamza T <thamza@192.168.1.16>
2023-08-10 16:28:29 +0100

    video creation working, code unclean

--- src/pages/projects/[projectId]/videos.tsx
+++ src/pages/projects/[projectId]/videos.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { api, type RouterOutputs } from "../../../utils/api";
+import ReactMarkdown from "react-markdown";
 import { abbreviateText } from "../../../utils/helpers";
 import { useRouter } from "next/router";
 
@@ -37,6 +38,11 @@ const VideosPage: NextPage = () => {
   const { data: sessionData } = useSession();
   const router = useRouter();
   const projectId = router.query.projectId as string;
+
+  const [expandedContent, setExpandedContent] = useState<{
+    content: string;
+    type: "script" | "storyboard";
+  } | null>(null);
   const [videos, setVideos] = useState<VideoSchema[]>([]);
 
   const [hasFetched, setHasFetched] = useState(false);
@@ -53,35 +59,70 @@ const VideosPage: NextPage = () => {
     }
   );
 
+  const handleExpandContent = (
+    content: string,
+    type: "script" | "storyboard"
+  ) => {
+    setExpandedContent({ content, type });
+  };
+
   return (
     <>
       <main>
         <div className="ml-72 mt-20">
           <h1 className="text-3xl font-bold">Videos</h1>
-          <div className="video-list-container">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
             {videos.map((video) => (
               <div key={video.id} className="card mr-8 w-96 bg-white shadow-xl">
                 <div className="card-body">
                   {/* Display the Video Script */}
                   <div>
                     <h2 className="text-lg font-bold">Script</h2>
-                    <p>
-                      {abbreviateText(video.scripts?.[0]?.content || "", 100)}
-                    </p>
-                    {/* Add an expand button if the content is long and you want to display it in a modal or popup */}
+                    <div className="flex items-center justify-between">
+                      {" "}
+                      {/* Add flex container */}
+                      <p>
+                        {abbreviateText(video.scripts?.[0]?.content || "", 100)}
+                      </p>
+                      <button
+                        className="bg-dark hover:bg-dark-700 rounded-6 btn ml-4 text-white"
+                        onClick={() =>
+                          handleExpandContent(
+                            video.scripts?.[0]?.content || "",
+                            "script"
+                          )
+                        }
+                      >
+                        Expand
+                      </button>
+                    </div>
                   </div>
                   <hr className="my-4" />{" "}
                   {/* Divider line between script and storyboard */}
                   {/* Display the Storyboard */}
                   <div>
                     <h2 className="text-lg font-bold">Storyboard</h2>
-                    <p>
-                      {abbreviateText(
-                        video.storyboards?.[0]?.content || "",
-                        100
-                      )}
-                    </p>
-                    {/* Similarly, add an expand button if the storyboard is long and you want to display it in a modal or popup */}
+                    <div className="flex items-center justify-between">
+                      {" "}
+                      {/* Add flex container */}
+                      <p>
+                        {abbreviateText(
+                          video.storyboards?.[0]?.content || "",
+                          100
+                        )}
+                      </p>
+                      <button
+                        className="bg-dark hover:bg-dark-700 rounded-6 btn ml-4 text-white"
+                        onClick={() =>
+                          handleExpandContent(
+                            video.storyboards?.[0]?.content || "",
+                            "storyboard"
+                          )
+                        }
+                      >
+                        Expand
+                      </button>
+                    </div>
                   </div>
                   {/* Add any other buttons or actions you might need for each video card */}
                   <div className="card-actions mt-8 justify-between">
@@ -91,6 +132,23 @@ const VideosPage: NextPage = () => {
               </div>
             ))}
           </div>
+          {expandedContent && (
+            <div className="modal modal-open">
+              <div className="modal-box">
+                <ReactMarkdown skipHtml={false}>
+                  {expandedContent.content}
+                </ReactMarkdown>
+                <div className="modal-action">
+                  <button
+                    className="btn text-white"
+                    onClick={() => setExpandedContent(null)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </>
