27f6926335a64e7719ef00ce7fa41d7920f2dfd7
Hamza T <thamza@192.168.1.16>
2023-08-11 21:15:37 +0100

    upgrading graph

--- src/server/graph/priorityQueue.ts
+++ src/server/graph/priorityQueue.ts
@@ -1,38 +1,85 @@
-class PriorityQueue {
-  nodes: Set<string> = new Set();
-  distances: Map<string, number> = new Map();
+type QueueValue = {
+  node: string;
+  distance: number;
+};
 
-  enqueue(node: string, distance: number) {
-    const existingDistance = this.distances.get(node) || Infinity;
-    if (distance < existingDistance) {
-      this.distances.set(node, distance);
+class PriorityQueue {
+  private values: QueueValue[] = [];
+
+  enqueue(node: string, distance: number): void {
+    this.values.push({ node, distance });
+    this.bubbleUp();
+  }
+
+  private bubbleUp(): void {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    if (!element) return;
+
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (!parent) break;
+
+      if (element.distance >= parent.distance) break;
+
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
     }
-    this.nodes.add(node);
   }
 
   dequeue(): string | undefined {
-    let minNode: string | undefined;
-    let minDistance = Infinity;
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (!min) return;
 
-    for (const node of this.nodes) {
-      const distance = this.distances.get(node) || Infinity;
-      console.log("Distances in PQ:", [...this.distances]);
+    if (this.values.length > 0 && end) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
 
-      if (distance < minDistance) {
-        minDistance = distance;
-        minNode = node;
+    return min.node;
+  }
+
+  private sinkDown(): void {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    if (!element) return;
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let swap: number | null = null;
+
+      let leftChild = this.values[leftChildIdx];
+      let rightChild = this.values[rightChildIdx];
+
+      if (leftChild && leftChild.distance < element.distance) {
+        swap = leftChildIdx;
       }
-    }
 
-    if (minNode !== undefined) {
-      this.nodes.delete(minNode);
-    }
+      if (
+        rightChild &&
+        ((swap === null && rightChild.distance < element.distance) ||
+          (swap !== null &&
+            leftChild &&
+            rightChild.distance < leftChild.distance))
+      ) {
+        swap = rightChildIdx;
+      }
 
-    return minNode;
+      if (swap === null) break;
+
+      this.values[idx] = this.values[swap] as QueueValue;
+      this.values[swap] = element;
+      idx = swap;
+    }
   }
 
   isEmpty(): boolean {
-    return this.nodes.size === 0;
+    return this.values.length === 0;
   }
 }
 
